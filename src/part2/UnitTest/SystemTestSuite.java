035698c9677006a79d928bacd2cba5bd248fa4cb
IsmaelHG <juegoseverywhereloquendo@gmail.com>
2021-01-08 11:08:16 +0100

    final?

--- src/part2/UnitTest/SystemTestSuite.java
+++
@@ -1,9 +0,0 @@
-package part2.UnitTest;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(value=Suite.class)
-@SuiteClasses(value={SystemTestSuite.class})
-public class SystemTestSuite {}
